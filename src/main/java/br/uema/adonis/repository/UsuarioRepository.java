b08cb06213c8cddc3088242c91f120cbbcbbf408
Pedro Vitor <pedrovitor.deva@gmail.com>
2026-06-11 13:49:20 -0300

    Remove @Repository annotation from UsuarioRepository for cleaner code

--- src/main/java/br/uema/adonis/repository/UsuarioRepository.java
+++ src/main/java/br/uema/adonis/repository/UsuarioRepository.java
@@ -3,8 +3,6 @@ package br.uema.adonis.repository;
 import br.uema.adonis.model.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 }
