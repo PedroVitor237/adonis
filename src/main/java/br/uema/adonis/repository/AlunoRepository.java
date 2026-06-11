0ef1eeb01699cdbec445f669b1e4ea508bf64349
Pedro Vitor <pedrovitor.deva@gmail.com>
2026-06-11 13:48:53 -0300

    Remove @Repository annotation from repository interfaces for cleaner code

--- src/main/java/br/uema/adonis/repository/AlunoRepository.java
+++ src/main/java/br/uema/adonis/repository/AlunoRepository.java
@@ -2,8 +2,5 @@ package br.uema.adonis.repository;
 
 import br.uema.adonis.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
 }
--- src/main/java/br/uema/adonis/repository/TreinoRepository.java
+++ src/main/java/br/uema/adonis/repository/TreinoRepository.java
@@ -1,11 +1,9 @@
 package br.uema.adonis.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.uema.adonis.model.Treino;
 
-@Repository
 public interface TreinoRepository 
     extends JpaRepository<Treino, Integer> {
 }
